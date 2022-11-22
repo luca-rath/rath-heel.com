e9a66ab9a992d55cc31e30bd7b37bd9044509063
Luca Rath-Heel <luca@sulu.io>
2022-11-22 09:51:24 +0100

    Fix node version and runtime

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ const nextConfig = {
     swcMinify: true,
     experimental: {
         appDir: true,
+        runtime: 'experimental-edge',
     },
     webpack(config) {
         config.module.rules.push({
