f6ba789fd9a6b2836fe0610ac963fd2f0f919e6b
Luca Rath-Heel <luca@sulu.io>
2022-11-22 18:32:59 +0100

    Fix image for cloudflare

--- components/atoms/image.tsx
+++ components/atoms/image.tsx
@@ -4,7 +4,7 @@ import { default as NextImage, ImageLoader } from 'next/image';
 
 let fallbackLoader: ImageLoader | undefined = undefined;
 
-if (process.env.HOSTER === 'cloudflare') {
+if (process.env.NEXT_PUBLIC_HOSTING_PROVIDER === 'cloudflare') {
     const normalizeSrc = (src: string) => {
         return src.startsWith('/') ? src.slice(1) : src;
     };
