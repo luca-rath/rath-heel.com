629a3b3d6efc746e11512266e2d6b47ad8f981f9
Luca Rath-Heel <luca@sulu.io>
2022-11-22 14:05:19 +0100

    Switch to edge runtime for contact api

--- pages/api/contact.ts
+++ pages/api/contact.ts
@@ -1,15 +1,26 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-import { z } from 'zod';
+import { NextRequest } from 'next/server';
+import { zfd } from 'zod-form-data';
 
-const schema = z.object({
-    firstName: z.string().optional(),
-    lastName: z.string().optional(),
-    email: z.string().email(),
-    message: z.string(),
+export const config = {
+    runtime: 'experimental-edge',
+};
+
+const schema = zfd.formData({
+    firstName: zfd.text().optional(),
+    lastName: zfd.text().optional(),
+    email: zfd.text(),
+    message: zfd.text(),
 });
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-    // TODO send confirmation email
-    const payload = schema.parse(req.body);
-    res.status(200).json(payload);
+export default async function handler(req: NextRequest) {
+    if (req.method !== 'POST') return new Response(null, { status: 404, statusText: 'Not Found' });
+
+    try {
+        const formData = await req.formData();
+        const data = schema.parse(formData);
+
+        return new Response(JSON.stringify(data), { status: 200 });
+    } catch (e) {
+        return new Response(null, { status: 400, statusText: 'Bad Request' });
+    }
 }
