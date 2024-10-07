6c976082e7cf8cd438dbcedbe401aa0d4b9626ff
Luca Rath-Heel <luca.rath-heel@geoinfo.ch>
2024-10-07 22:44:18 +0200

    fixes

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,5 @@
 import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
+import { EnvironmentPlugin } from 'webpack';
 
 // Here we use the @cloudflare/next-on-pages next-dev module to allow us to use bindings during local development
 // (when running the application with `next dev`), for more information see:
@@ -10,6 +11,7 @@ if (process.env.NODE_ENV === 'development') {
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     webpack(config) {
+        config.plugins.push(new EnvironmentPlugin(['NEXT_PUBLIC_TURNSTILE_SITE_KEY']));
         config.module.rules.push({
             test: /\.svg$/i,
             use: ['@svgr/webpack'],
